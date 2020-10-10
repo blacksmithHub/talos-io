7db47fa4225cd2e946782441f8307b6c618ded15
Danniel Isiah J. Libor <disaiahlibor@gmail.com>
2020-10-10 18:02:57 +0800

    remove devtool

--- src/background.js
+++ src/background.js
@@ -82,7 +82,7 @@ function createWindow () {
     monitorWin.loadURL(`${process.env.WEBPACK_DEV_SERVER_URL}/#/monitor`)
     settingsWin.loadURL(`${process.env.WEBPACK_DEV_SERVER_URL}/#/settings`)
 
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    // if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
@@ -157,7 +157,7 @@ if (isDevelopment) {
 ipcMain.on('toggle-monitor', (event, arg) => {
   monitorWin.show()
 
-  if (!process.env.IS_TEST) monitorWin.openDevTools()
+  // if (!process.env.IS_TEST) monitorWin.openDevTools()
 
   monitorWin.webContents.send('init', arg)
 })
@@ -165,5 +165,5 @@ ipcMain.on('toggle-monitor', (event, arg) => {
 ipcMain.on('toggle-settings', (event, arg) => {
   settingsWin.show()
 
-  if (!process.env.IS_TEST) settingsWin.openDevTools()
+  // if (!process.env.IS_TEST) settingsWin.openDevTools()
 })
