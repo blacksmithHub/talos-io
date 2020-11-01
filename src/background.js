d7e48e63b9042b3d4c87415efb73d448828fb4cb
Danniel Isiah J. Libor <disaiahlibor@gmail.com>
2020-11-01 19:14:45 +0800

    autoupdater

--- src/background.js
+++ src/background.js
@@ -3,6 +3,7 @@
 import { app, protocol, BrowserWindow, ipcMain, globalShortcut } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import { autoUpdater } from 'electron-updater'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -48,6 +49,7 @@ function createWindow () {
   } else {
     createProtocol('app')
     win.loadURL('app://./index.html')
+    autoUpdater.checkForUpdatesAndNotify()
   }
 
   win.on('closed', () => {
--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,24 @@ module.exports = {
   },
   pluginOptions: {
     electronBuilder: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      builderOptions: {
+        nsis: {
+          oneClick: false,
+          allowToChangeInstallationDirectory: true
+        },
+        publish: [
+          {
+            provider: 'github',
+            repo: 'titan-bot-frontend',
+            owner: 'dannielibor',
+            token: '03c59d13a882d93d7a8f50772e1944791d46eb8e',
+            releaseType: 'draft',
+            publishAutoUpdate: true,
+            private: true
+          }
+        ]
+      }
     }
   }
 }
