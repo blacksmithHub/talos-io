d893487a1622cef642725e607d0af3f9df5793d9
Danniel Isiah J. Libor <disaiahlibor@gmail.com>
2020-12-15 21:08:29 +0800

    code adjustment

--- server/api/order.js
+++ server/api/order.js
@@ -2,90 +2,85 @@ const express = require('express')
 
 const router = express.Router()
 
-const axios = require('axios').default
-const axiosCookieJarSupport = require('axios-cookiejar-support').default
-const tough = require('tough-cookie')
-const qs = require('qs')
-
 /**
  * Place order
  */
 router.post('/', async (req, res) => {
-  axiosCookieJarSupport(axios)
-  const cookieJar = new tough.CookieJar()
+  let request = require('request')
 
-  const options = {}
+  const option = {}
 
-  const proxy = req.body.proxy
+  if (req.body.proxy) option.proxy = 'http://run:Lw6WS7kj@62.133.48.22:59518'
 
-  if (proxy) {
-    options.proxy = {
-      host: proxy.host,
-      port: proxy.port,
-      auth: {
-        username: proxy.username,
-        password: proxy.password
-      }
-    }
+  request = request.defaults(option)
+
+  const jar = request.jar()
+
+  const placeOrder = {
+    uri: 'https://www.titan22.com/rest/V1/carts/mine/payment-information',
+    body: JSON.stringify(req.body.payload),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${req.body.token}`
+    },
+    jar
   }
 
-  const http = axios.create(options)
+  await request(placeOrder, async function (error, response) {
+    if (error || response.statusCode !== 200) res.status(response.statusCode).send({})
 
-  http.interceptors.request.use(async config => {
-    /**
-     * Set Headers config
-     */
-    config.headers.Accept = 'application/json'
-    if (req.body.token) config.headers.Authorization = `Bearer ${req.body.token}`
+    const getTransactionData = {
+      uri: 'https://www.titan22.com/ccpp/htmlredirect/gettransactiondata',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      jar
+    }
 
-    return config
-  })
+    await request(getTransactionData, async function (error, response) {
+      if (error || response.statusCode !== 200) res.status(response.statusCode).send({})
 
-  const stepOne = await http.post('https://www.titan22.com/rest/V1/carts/mine/payment-information', req.body.payload, { jar: cookieJar, withCredentials: true })
-    .then((response) => response)
-    .catch(({ response }) => response)
-
-  if (stepOne.status === 200) {
-    const stepTwo = await http.get('https://www.titan22.com/ccpp/htmlredirect/gettransactiondata', { jar: cookieJar, withCredentials: true })
-      .then((response) => response)
-      .catch(({ response }) => response)
-
-    if (stepTwo.status === 200) {
       const parameters = {}
-      const fieldRecords = stepTwo.data.fields
-      const valueRecords = stepTwo.data.values
+      const fieldRecords = JSON.parse(response.body).fields
+      const valueRecords = JSON.parse(response.body).values
 
       for (let index = 0; index < fieldRecords.length; index++) {
         parameters[fieldRecords[index]] = valueRecords[index]
       }
 
-      const http = axios.create(options)
-
-      http.interceptors.request.use(async config => {
-        config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-        return config
-      })
-
-      await http.post('https://t.2c2p.com/RedirectV3/Payment', qs.stringify(parameters), { jar: cookieJar, withCredentials: true })
-        .then((response) => response)
-        .catch(({ response }) => response)
-
-      const cookie = cookieJar.store.idx['2c2p.com']['/']['ASP.NET_SessionId']
-
-      res.status(200).send({
-        cookies: {
-          name: 'ASP.NET_SessionId',
-          value: cookie.value,
-          domain: '.2c2p.com'
+      const payment = {
+        uri: 'https://t.2c2p.com/RedirectV3/Payment',
+        form: parameters,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
         },
-        data: parameters
+        jar
+      }
+
+      await request(payment, function (error, response) {
+        if (error) res.status(500).send({})
+
+        jar._jar.store.getAllCookies(function (err, cookieArray) {
+          if (err) res.status(500).send({})
+
+          const collection = cookieArray.find((val) => val.key === 'ASP.NET_SessionId')
+
+          res.status(200).send({
+            cookies: {
+              name: 'ASP.NET_SessionId',
+              value: collection.value,
+              domain: '.2c2p.com',
+              expiry: collection.expiry
+            },
+            data: parameters
+          })
+        })
       })
-    } else {
-      res.status(stepOne.status).send({})
-    }
-  } else {
-    res.status(stepOne.status).send({})
-  }
+    })
+  })
 })
 
 module.exports = router
