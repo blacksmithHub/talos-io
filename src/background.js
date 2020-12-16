f0cbd5f7c81f1abb16dba8f81c14b06a39aec9e2
Danniel Isiah J. Libor <disaiahlibor@gmail.com>
2020-12-16 20:41:39 +0800

    puppeteer fix

--- src/background.js
+++ src/background.js
@@ -255,7 +255,7 @@ ipcMain.on('bind', (event, arg) => {
 /**
  * paypal checkout method
  */
-ipcMain.on('pay-with-paypal', (event, arg) => {
+ipcMain.on('pay-with-paypal', async (event, arg) => {
   const task = JSON.parse(arg).task
 
   let proxy = {}
@@ -263,65 +263,62 @@ ipcMain.on('pay-with-paypal', (event, arg) => {
   if (task.proxy && Object.keys(task.proxy).length) proxy = task.proxy.proxies[Math.floor(Math.random() * task.proxy.proxies.length)]
 
   const puppeteer = require('puppeteer')
-  const proxyChain = require('proxy-chain');
+  const proxyChain = require('proxy-chain')
 
-  (async () => {
-    const args = ['--window-size=800,600']
+  const args = ['--window-size=800,600']
 
-    if (Object.keys(proxy).length) {
-      const oldProxyUrl = `http://${proxy.username}:${proxy.password}@${proxy.host}:${proxy.port}`
-      const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl)
-      args.push(`--proxy-server=${newProxyUrl}`)
-    }
+  if (Object.keys(proxy).length) {
+    const oldProxyUrl = `http://${proxy.username}:${proxy.password}@${proxy.host}:${proxy.port}`
+    const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl)
+    args.push(`--proxy-server=${newProxyUrl}`)
+  }
 
-    const browser = await puppeteer.launch({
-      headless: false,
-      executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
-      args: args,
-      defaultViewport: null
-    })
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: args,
+    defaultViewport: null
+  })
 
-    const page = await browser.newPage()
+  const page = await browser.newPage()
 
-    await page.setRequestInterception(true)
+  await page.setRequestInterception(true)
 
-    page.on('request', (request) => {
-      const url = request.url()
-      const resourceType = request.resourceType()
+  page.on('request', (request) => {
+    const url = request.url()
+    const resourceType = request.resourceType()
 
-      const filters = ['queue-it.net']
+    const filters = ['queue-it.net']
 
-      const shouldAbort = filters.some((urlPart) => url.includes(urlPart))
+    const shouldAbort = filters.some((urlPart) => url.includes(urlPart))
 
-      if (shouldAbort ||
+    if (shouldAbort ||
       resourceType === 'media' ||
       url.endsWith('.png') ||
       url.endsWith('.gif') ||
       url.endsWith('.jpg')
-      ) {
-        request.abort()
-      } else {
-        request.continue()
-      }
-    })
+    ) {
+      request.abort()
+    } else {
+      request.continue()
+    }
+  })
 
-    page.on('requestfinished', async (request) => {
-      if (request.url() === 'https://www.titan22.com/rest/V1/carts/mine/payment-information') MainWindow.getWindow().webContents.send('updateTask', task)
-    })
+  page.on('requestfinished', async (request) => {
+    if (request.url() === 'https://www.titan22.com/rest/V1/carts/mine/payment-information') MainWindow.getWindow().webContents.send('updateTask', task)
+  })
 
-    await page.goto('https://www.titan22.com/customer/account/login/')
-    await page.type('#email', task.profile.email)
-    await page.type('#pass', task.profile.password)
-    await page.click('#send2')
-    await page.waitForNavigation()
-    await page.goto('https://www.titan22.com/checkout/')
-  })()
+  await page.goto('https://www.titan22.com/customer/account/login/')
+  await page.type('#email', task.profile.email)
+  await page.type('#pass', task.profile.password)
+  await page.click('#send2')
+  await page.waitForNavigation()
+  await page.goto('https://www.titan22.com/checkout/')
 })
 
 /**
  * 2c2p checkout method
  */
-ipcMain.on('pay-with-2c2p', (event, arg) => {
+ipcMain.on('pay-with-2c2p', async (event, arg) => {
   const task = JSON.parse(arg).task
 
   let proxy = {}
@@ -330,95 +327,92 @@ ipcMain.on('pay-with-2c2p', (event, arg) => {
 
   const settings = JSON.parse(arg).settings
   const puppeteer = require('puppeteer')
-  const proxyChain = require('proxy-chain');
+  const proxyChain = require('proxy-chain')
 
-  (async () => {
-    const args = ['--window-size=800,600']
+  const args = ['--window-size=800,600']
 
-    if (Object.keys(proxy).length) {
-      const oldProxyUrl = `http://${proxy.username}:${proxy.password}@${proxy.host}:${proxy.port}`
-      const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl)
-      args.push(`--proxy-server=${newProxyUrl}`)
-    }
+  if (Object.keys(proxy).length) {
+    const oldProxyUrl = `http://${proxy.username}:${proxy.password}@${proxy.host}:${proxy.port}`
+    const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl)
+    args.push(`--proxy-server=${newProxyUrl}`)
+  }
 
-    const browser = await puppeteer.launch({
-      headless: false,
-      executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
-      args: args,
-      defaultViewport: null
-    })
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: args,
+    defaultViewport: null
+  })
 
-    const page = await browser.newPage()
+  const page = await browser.newPage()
 
-    await page.setCookie({
-      name: task.transactionData.cookies.name,
-      value: task.transactionData.cookies.value,
-      domain: task.transactionData.cookies.domain
-    })
+  await page.setCookie({
+    name: task.transactionData.cookies.name,
+    value: task.transactionData.cookies.value,
+    domain: task.transactionData.cookies.domain
+  })
 
-    await page.goto('https://t.2c2p.com/RedirectV3/Payment/Accept')
+  await page.goto('https://t.2c2p.com/RedirectV3/Payment/Accept')
 
-    const array = [
+  const array = [
       `<p><strong>Task:</strong> ${task.name}</p>`,
       `<p><strong>Profile:</strong> ${task.profile.name}</p>`,
       `<p><strong>Product name:</strong> ${task.transactionData.order.name}</p>`,
       `<p><strong>Product SKU:</strong> ${task.transactionData.order.sku}</p>`,
       `<p><strong>Size:</strong> ${task.transactionData.order.sizeLabel}</p>`,
       `<p><strong>Price:</strong> ${task.transactionData.order.price.toLocaleString()}</p>`
-    ]
+  ]
 
-    await page.waitForSelector('.navbar-inner')
+  await page.waitForSelector('.navbar-inner')
 
-    await page.evaluate((array) => {
-      array.forEach(element => {
-        var div = document.getElementsByClassName('navbar-inner')[0]
-        div.insertAdjacentHTML('beforeend', element)
-      })
-    }, array)
-
-    if (task.bank && Object.keys(task.bank).length) {
-      switch (task.bank.bank.toLowerCase()) {
-        case 'gcash':
-          if (settings.autoPay || settings.autoFill) {
-            await page.click('#btnGCashSubmit')
-            await page.waitForNavigation()
-            await page.waitForSelector('.layout-header')
-
-            await page.evaluate((array) => {
-              array.forEach(element => {
-                var div = document.getElementsByClassName('layout-header')[0]
-                div.insertAdjacentHTML('beforebegin', element)
-              })
-            }, array)
-
-            await page.type('input[type=number]', task.bank.cardNumber)
-            await page.click('.ap-button')
-          }
-          break
-
-        default:
-          if (settings.autoPay) {
-            await page.type('#credit_card_number', task.bank.cardNumber)
-            await page.type('#credit_card_holder_name', task.bank.cardHolder)
-            await page.type('#credit_card_expiry_month', task.bank.expiryMonth)
-            await page.type('#credit_card_expiry_year', task.bank.expiryYear)
-            await page.type('#credit_card_cvv', task.bank.cvv)
-            await page.type('#credit_card_issuing_bank_name', task.bank.bank)
-            await page.click('#btnCCSubmit')
-          } else if (settings.autoFill) {
-            await page.type('#credit_card_number', task.bank.cardNumber)
-            await page.type('#credit_card_holder_name', task.bank.cardHolder)
-            await page.type('#credit_card_expiry_month', task.bank.expiryMonth)
-            await page.type('#credit_card_expiry_year', task.bank.expiryYear)
-            await page.type('#credit_card_cvv', task.bank.cvv)
-            await page.type('#credit_card_issuing_bank_name', task.bank.bank)
-          }
-          break
-      }
-    }
-
-    page.on('close', () => {
-      MainWindow.getWindow().webContents.send('updateTask', task)
+  await page.evaluate((array) => {
+    array.forEach(element => {
+      var div = document.getElementsByClassName('navbar-inner')[0]
+      div.insertAdjacentHTML('beforeend', element)
     })
-  })()
+  }, array)
+
+  if (task.bank && Object.keys(task.bank).length) {
+    switch (task.bank.bank.toLowerCase()) {
+      case 'gcash':
+        if (settings.autoPay || settings.autoFill) {
+          await page.click('#btnGCashSubmit')
+          await page.waitForNavigation()
+          await page.waitForSelector('.layout-header')
+
+          await page.evaluate((array) => {
+            array.forEach(element => {
+              var div = document.getElementsByClassName('layout-header')[0]
+              div.insertAdjacentHTML('beforebegin', element)
+            })
+          }, array)
+
+          await page.type('input[type=number]', task.bank.cardNumber)
+          await page.click('.ap-button')
+        }
+        break
+
+      default:
+        if (settings.autoPay) {
+          await page.type('#credit_card_number', task.bank.cardNumber)
+          await page.type('#credit_card_holder_name', task.bank.cardHolder)
+          await page.type('#credit_card_expiry_month', task.bank.expiryMonth)
+          await page.type('#credit_card_expiry_year', task.bank.expiryYear)
+          await page.type('#credit_card_cvv', task.bank.cvv)
+          await page.type('#credit_card_issuing_bank_name', task.bank.bank)
+          await page.click('#btnCCSubmit')
+        } else if (settings.autoFill) {
+          await page.type('#credit_card_number', task.bank.cardNumber)
+          await page.type('#credit_card_holder_name', task.bank.cardHolder)
+          await page.type('#credit_card_expiry_month', task.bank.expiryMonth)
+          await page.type('#credit_card_expiry_year', task.bank.expiryYear)
+          await page.type('#credit_card_cvv', task.bank.cvv)
+          await page.type('#credit_card_issuing_bank_name', task.bank.bank)
+        }
+        break
+    }
+  }
+
+  page.on('close', () => {
+    MainWindow.getWindow().webContents.send('updateTask', task)
+  })
 })
